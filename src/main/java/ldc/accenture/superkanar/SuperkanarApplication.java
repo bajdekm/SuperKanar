c4ff21c8855b17c24d04ba7604756e90778182a0
Michal <michal@bajdek.pl>
2017-09-25 20:06:11 +0200

    init repo

---
+++ src/main/java/ldc/accenture/superkanar/SuperkanarApplication.java
@@ -0,0 +1,12 @@
+package ldc.accenture.superkanar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperkanarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperkanarApplication.class, args);
+	}
+}
